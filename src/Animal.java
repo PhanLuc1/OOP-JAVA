ad94ac77ba7d481de7479836911fd02617f1505f
Phanluc1 <lucptt.22it@vku.udn.vn>
2024-07-23 03:41:20 +0700

    test

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    private String species;
+    public Animal(String species) {
+        this.species = species;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("Bird");
+        animal.setSpecies("Snake");
+        System.out.println(animal.getSpecies());
     }
 }
